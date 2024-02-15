0a67c7cccd7d3e38580ddf8306d8d125b200ff61
Pascal1400 <1039861@hr.nl>
2024-02-15 21:27:20 +0100

    start 10 films

--- webservice/index.php
+++ webservice/index.php
@@ -13,3 +13,52 @@ if (!isset($_GET['id'])) {
 header("Content-Type: application/json");
 echo json_encode($data);
 exit;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upcoming movie magazine</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="application/javascript" src="js/main.js" defer></script>
+</head>
+<body>
+<header>
+    <nav>
+        <a href="#">about</a>
+        <img src="" alt="Logo">
+        <a href="#">IMDb</a>
+    </nav>
+</header>
+<main>
+    <h1>Top upcoming movies</h1>
+    <section>
+        <div>
+            <div>
+                <img src="" alt="foto film">
+            </div>
+            <div>
+        </div>
+                <h2>Film name</h2>
+                <p>Release date</p>
+            </div>
+        <div>
+            <div>
+                <p>Description</p>
+            </div>
+            <ul>
+                <li>Genres:</li>
+                <li>...</li>
+            </ul>
+
+        </div>
+    </section>
+</main>
+<footer>
+    <img src="" alt="Logo">
+</footer>
+</body>
+</html>
